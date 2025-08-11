b19b2c0eab8c5934158a6f2c2fb3ca207b8dd746
OmarEhab14 <www.omarehab3710@gmail.com>
2025-08-11 21:00:20 +0300

    Green: increase hours if minutes reached 60 requirement

--- tdd-fawry-task/src/main/java/org/example/StopWatch.java
+++ tdd-fawry-task/src/main/java/org/example/StopWatch.java
@@ -2,13 +2,18 @@ package org.example;
 
 public class StopWatch {
     private int minutes;
+    private int hours;
 
     public void record(int minutes) {
         if (minutes >= 0) {
-            this.minutes = minutes;
+            this.hours += (this.minutes + minutes) / 60;
+            this.minutes += (this.minutes + minutes) % 60;
         }
     }
     public int getMinutes() {
         return minutes;
+    }
+    public int getHours() {
+        return hours;
     }
 }
