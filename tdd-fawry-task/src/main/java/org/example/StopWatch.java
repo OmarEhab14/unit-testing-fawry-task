9621c43dc147f4b8973e858feac25b73b4aa4e68
OmarEhab14 <www.omarehab3710@gmail.com>
2025-08-11 20:45:35 +0300

    Green: record minutes requirement

--- tdd-fawry-task/src/main/java/org/example/StopWatch.java
+++ tdd-fawry-task/src/main/java/org/example/StopWatch.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class StopWatch {
+    private int minutes;
+
+    public void record(int minutes) {
+        this.minutes = minutes;
+    }
+    public int getMinutes() {
+        return minutes;
+    }
 }
