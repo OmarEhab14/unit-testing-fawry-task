e9549851635d6179c3a097dc634a148bcd801b46
OmarEhab14 <www.omarehab3710@gmail.com>
2025-08-11 21:01:59 +0300

    Refactor: increase hours if minutes reached 60 requirement

--- tdd-fawry-task/src/main/java/org/example/StopWatch.java
+++ tdd-fawry-task/src/main/java/org/example/StopWatch.java
@@ -5,10 +5,9 @@ public class StopWatch {
     private int hours;
 
     public void record(int minutes) {
-        if (minutes >= 0) {
-            this.hours += (this.minutes + minutes) / 60;
-            this.minutes += (this.minutes + minutes) % 60;
-        }
+        if (minutes < 0) return;
+        this.hours += (this.minutes + minutes) / 60;
+        this.minutes += (this.minutes + minutes) % 60;
     }
     public int getMinutes() {
         return minutes;
