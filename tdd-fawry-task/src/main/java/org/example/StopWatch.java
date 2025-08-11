c9c3524e646e0fc16c682a87c9d25de1969fb809
OmarEhab14 <www.omarehab3710@gmail.com>
2025-08-11 20:47:56 +0300

    Green: ignore negative input requirement

--- tdd-fawry-task/src/main/java/org/example/StopWatch.java
+++ tdd-fawry-task/src/main/java/org/example/StopWatch.java
@@ -4,6 +4,9 @@ public class StopWatch {
     private int minutes;
 
     public void record(int minutes) {
+        if (minutes < 0) {
+            return;
+        }
         this.minutes = minutes;
     }
     public int getMinutes() {
